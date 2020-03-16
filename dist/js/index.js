c7fa0ead71da259ad764db106f85d32a5f4a8fcb
Svetlana <VSveta.83@gmail.com>
2020-03-16 19:05:53 +0200

    change dist

--- dist/js/index.js
+++ dist/js/index.js
@@ -6,9 +6,9 @@ $(document).ready(function() {
     speed: 1200,
     // adaptiveHeight: true,
     prevArrow:
-      '<button type="button" class="slick-prev"><img src="../icons/chevron-left-solid.png"/></button>',
+      '<button type="button" class="slick-prev"><img src="../src/icons/chevron-left-solid.png"/></button>',
     nextArrow:
-      '<button type="button" class="slick-next"><img src="../icons/chevron-right-solid.png"/> </button>',
+      '<button type="button" class="slick-next"><img src="../src/icons/chevron-right-solid.png"/> </button>',
     responsive: [
       {
         breakpoint: 1024,

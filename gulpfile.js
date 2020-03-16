749acada5fbb8480683b71f216fe229941f70852
Svetlana <VSveta.83@gmail.com>
2020-03-16 18:55:42 +0200

    pulse

---
+++ gulpfile.js
@@ -0,0 +1,79 @@
+const gulp = require('gulp');
+const browserSync = require('browser-sync');
+const sass = require('gulp-sass');
+const rename = require('gulp-rename');
+const autoprefixer = require('gulp-autoprefixer');
+const cleanCSS = require('gulp-clean-css');
+const htmlmin = require('gulp-htmlmin');
+const imagemin = require('gulp-imagemin');
+// Static server
+gulp.task('server', function() {
+  browserSync.init({
+    server: {
+      baseDir: 'dist'
+    }
+  });
+  gulp.watch('src/*.html').on('change', browserSync.reload);
+});
+//task for  work with style
+gulp.task('styles', function() {
+  return gulp
+    .src('src/sass/**/*.+(scss|sass)')
+    .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(
+      autoprefixer({
+        cascade: false
+      })
+    )
+    .pipe(cleanCSS({ compatibility: 'ie8' }))
+    .pipe(gulp.dest('dist/css'))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('html', function() {
+  return gulp
+    .src('src/*.html')
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(gulp.dest('dist/'));
+});
+
+gulp.task('script', function() {
+  return gulp.src('src/js/**/*.js').pipe(gulp.dest('dist/js'));
+});
+gulp.task('fonts', function() {
+  return gulp.src('src/fonts/**/*').pipe(gulp.dest('dist/fonts'));
+});
+
+gulp.task('icons', function() {
+  return gulp.src('src/icons/**/*').pipe(gulp.dest('dist/icons'));
+});
+gulp.task('images', function() {
+  return gulp
+    .src('src/img/**/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('dist/img'));
+});
+gulp.task('mailer', function() {
+  return gulp.src('src/mailer/**/*').pipe(gulp.dest('dist/mailer'));
+});
+//watcher
+gulp.task('watch', function() {
+  gulp.watch('src/sass/**/*.+(scss|sass|css)', gulp.parallel('styles'));
+  gulp.watch('src/*.html').on('change', gulp.parallel('html'));
+});
+// add start default task
+gulp.task(
+  'default',
+  gulp.parallel(
+    'watch',
+    'server',
+    'styles',
+    'html',
+    'script',
+    'fonts',
+    'icons',
+    'images',
+    'mailer'
+  )
+);
---
+++ src/js/index.js
@@ -0,0 +1,144 @@
+'use strict';
+// slick-slider
+$(document).ready(function() {
+  //carousel
+  $('.carousel__inner').slick({
+    speed: 1200,
+    // adaptiveHeight: true,
+    prevArrow:
+      '<button type="button" class="slick-prev"><img src="../icons/chevron-left-solid.png"/></button>',
+    nextArrow:
+      '<button type="button" class="slick-next"><img src="../icons/chevron-right-solid.png"/> </button>',
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          dots: true,
+          arrows: false
+        }
+      }
+    ]
+  });
+  
+  //catalog tabs
+  $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function() {
+    $(this)
+      .addClass('catalog__tab_active')
+      .siblings()
+      .removeClass('catalog__tab_active')
+      .closest('div.container')
+      .find('div.catalog__content')
+      .removeClass('catalog__content_active')
+      .eq($(this).index())
+      .addClass('catalog__content_active');
+  });
+  // cards link
+  function changeClass(itemClass) {
+    $(itemClass).each(function(indx) {
+      $(this).on('click', function(event) {
+        event.preventDefault();
+        $('.catalog-item__front-side')
+          .eq(indx)
+          .toggleClass('catalog-item__front-side_active');
+        $('.catalog-item__back-side')
+          .eq(indx)
+          .toggleClass('catalog-item__back-side_active');
+      });
+    });
+  }
+  changeClass('.catalog-item__link');
+  changeClass('.catalog-item__back');
+
+  //Modal
+  $('[data-modal=consultation]').on('click', function() {
+    $('.overlay, #consultation').fadeIn();
+  });
+  $('.js-close').on('click', function() {
+    $('.overlay, #consultation, #thanks, #order').fadeOut();
+  });
+  $('.button_mini').each(function(i) {
+    $(this).on('click', function() {
+      $('#order .modal__subtitle').text(
+        $('.catalog-item__title')
+          .eq(i)
+          .text()
+      );
+      $('.overlay, #order').fadeIn();
+    });
+  });
+  // validation form
+  function validateForm(formId) {
+    $(formId).validate({
+      rules: {
+        name: 'required',
+        phone: { required: true, minlength: 10 },
+        email: { required: true, email: true }
+      },
+      messages: {
+        name: 'Пожалуйста введите Ваше имя',
+        phone: {
+          required: 'Пожалуйста введите Ваш телефон',
+          minlength: jQuery.validator.format(
+            'Номер телефона должен содержать {0}  цифр'
+          )
+        },
+        email: {
+          required: 'Пожалуйста введите Ваш e-mail',
+          email: 'Ваш email должен быть в формате name@domain.com'
+        }
+      }
+    });
+  }
+  validateForm('#consultation form');
+  validateForm('#consultation-form');
+  validateForm('#order form');
+  $('input[name=phone]').mask('+7(999) 999-99-99');
+
+  // sending form`s data
+  $('form').submit(function(e) {
+    e.preventDefault();
+    $.ajax({
+      type: 'POST',
+      url: 'mailer/smart.php',
+      data: $(this).serialize()
+    }).done(function() {
+      $(this)
+        .find('input')
+        .val('');
+      $('#consultation , #order').fadeOut();
+      $('.overlay , #thanks').fadeIn();
+      $('form').trigger('reset');
+    });
+    return false;
+  });
+  //scroll and page-up
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 900) {
+      $('.pageup').fadeIn();
+    } else {
+      $('.pageup').fadeOut();
+    }
+  });
+  $("a[href=#up]").click(function() {
+    const _href = $(this).attr('href');
+    $('html, body').animate({ scrollTop: $(_href).offset().top + 'px' });
+    return false;
+  });
+});
+
+//tyni-slider pure js
+// var slider = tns({
+//   container: '.carousel__inner',
+//   items: 1,
+//   slideBy: 'page',
+//   controls: false,
+//   nav: false
+// });
+
+// document.querySelector('.prev').addEventListener('click', function() {
+//   slider.goTo('prev');
+// });
+
+// document.querySelector('.next').addEventListener('click', function() {
+//   slider.goTo('next');
+// });
